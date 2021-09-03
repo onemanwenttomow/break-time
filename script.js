7706a1425d35a915ba8087a291f0ae968f306ebd
stefanavra <stefan.avra@gmail.com>
2021-09-03 17:26:29 +0200

    added color transition, fixed time format

--- script.js
+++ script.js
@@ -26,18 +26,31 @@ function addBreakEndTimeToHtml() {
     intervalId = setInterval(updateProgressBar, 1000);
     var meetBackHtml =
         '<h1>Meet back at: <span id="meetback">' +
-        `${end.getHours()}:${end.getMinutes()}` +
+        end.toLocaleTimeString("en-GB", {
+            hour: "2-digit",
+            minute: "2-digit",
+        }) +
         "</span></h1>";
     document.getElementById("meet-at").innerHTML = meetBackHtml;
 }
 
 function updateProgressBar() {
+    let bar = document.querySelector("#progress");
     let progress = mapNum(Date.now(), d.getTime(), end.getTime(), 100, 0);
+    bar.style.backgroundColor = `hsl(351.2, 100%, ${mapNum(
+        progress,
+        0,
+        100,
+        83.9,
+        100
+    )}%)`;
     if (progress <= 0) {
         clearInterval(intervalId);
+        bar.style.width = 0;
+        return;
     }
-    console.log("tick", `${progress}%`, Date.now(), d.getTime(), end.getTime());
-    document.querySelector("#progress").style.width = `${mapNum(
+    // console.log("tick", `${progress}%`, Date.now(), d.getTime(), end.getTime());
+    bar.style.width = `${mapNum(
         Date.now(),
         d.getTime(),
         end.getTime(),
--- todo.md
+++
@@ -1,9 +0,0 @@
--   prograss bar
-
-    -   white to highlight color when shorter
-    -   length of text container
-    -   stroke size of text
-    -   starting automatically when changing the timme?
-
--   https://www.youtube.com/watch?v=MCi6AZMkxcU
--   (convert logo to svg)
